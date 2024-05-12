d7a01e6f62dbea24877a291861ed9cb724cdf9f2
Danylo Krasii <krasiydm@gmail.com>
2024-05-12 17:24:43 +0300

    Fixed the code

--- windows_classifier/prepare_dataset.py
+++ windows_classifier/prepare_dataset.py
@@ -3,15 +3,12 @@ import os
 from PIL import Image
 
 def process_annotations(json_path, images_dir, output_dir):
-    # Завантаження даних аннотації
     with open(json_path, 'r') as f:
         data = json.load(f)
 
-    # Перевірка наявності каталогу виводу
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Список для збереження даних про зображення
     images_data = []
 
     for annotation in data['annotations']:
@@ -21,14 +18,14 @@ def process_annotations(json_path, images_dir, output_dir):
             try:
                 with Image.open(img_path) as img:
                     bbox = annotation['bbox']
-                    # Вирізання зображення за баундінг боксом
+     
                     cropped_image = img.crop((bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]))
-                    # Створення назви файлу для збереженого зображення
+          
                     output_file_name = f"{annotation['image_id']}_{annotation['id']}.png"
                     output_file_path = os.path.join(output_dir, output_file_name)
                     cropped_image.save(output_file_path)
                     
-                    # Записуємо інформацію для конфігураційного файлу
+            
                     images_data.append({
                         'id': annotation['id'],
                         'file_name': output_file_name,
@@ -37,12 +34,11 @@ def process_annotations(json_path, images_dir, output_dir):
             except Exception as e:
                 print(f"Error processing image {img_path}: {e}")
 
-    # Запис конфігураційного файлу
     with open(os.path.join(output_dir, 'config.json'), 'w') as f:
         json.dump(images_data, f, indent=4)
 
-# Використання функції
-json_path = 'init_dataset/annotations.json'
-images_dir = 'init_dataset/images'
-output_dir = 'dataset/train'
-process_annotations(json_path, images_dir, output_dir)
+if __name__ == "__main__":
+    json_path = 'init_dataset/annotations.json'
+    images_dir = 'init_dataset/images'
+    output_dir = 'dataset/train'
+    process_annotations(json_path, images_dir, output_dir)
